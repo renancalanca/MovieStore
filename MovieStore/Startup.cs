387055103ffd082e594f2fdebff273b769bc2efa
Renan Campregher <renan.calanca@hotmail.com>
2018-11-23 14:36:29 -0200

    Add project files.

---
+++ MovieStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieStore.Startup))]
+namespace MovieStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
